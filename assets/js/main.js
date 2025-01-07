65006efc60a72edb58d14bf9994a825a29b389ca
Geovanne19 <130259323+Geovanne19@users.noreply.github.com>
2025-01-07 18:04:42 -0300

    add responsividade

--- assets/js/main.js
+++ assets/js/main.js
@@ -102,3 +102,19 @@ function navHighlighter() {
     }
   });
 }
+
+const navMenu = document.getElementById('sidebar'),
+  navToggle = document.getElementById('nav-toggle'),
+  navClose = document.getElementById('nav-close') 
+
+if(navToggle) {
+  navToggle.addEventListener("click",()=> {
+    navMenu.classList.add('show-sidebar')
+  })
+}
+
+if(navClose) {
+  navClose.addEventListener("click",()=> {
+    navMenu.classList.remove('show-sidebar')
+  })
+}
